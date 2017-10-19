ed5bbf41d49d6ea47b729b6fb8b5158042d7403d
Cibele Paulino <cibelepaulino@rocketmail.com>
2017-10-19 19:14:40 -0300

    Entrada modificada

--- ep1.c
+++ ep1.c
@@ -4,34 +4,42 @@
 #include<locale.h>
 
 void conversao(){
-	
+	printf("Conversão\n");
+	esperar_comando();
+	chamar_menu();
 }
 
 void sistema_linear(){
 	
-}
-
-void equacao_algebrica(){
+	printf("Sistema Linear\n");
+	esperar_comando();
+	chamar_menu();
 	
 }
 
+void equacao_algebrica(){
+	printf("Equação Algebrica\n");
+	esperar_comando();
+	chamar_menu();
+}
+void esperar_comando(){
+	char entradaChr;
+	printf("Digite qualquer letra para continuar:\n");
+	scanf(" %c", &entradaChr);
+}
 void chamar_menu(){
-	//Função chama o meu da aplicação, solicita a opção desejada, ler a opção escolhida pelo usuário e 
-	//direciona para a opção desejada através do switch case.
-	char menu ;
+	//Função chama o menu da aplicação, solicita a opção desejada, ler a opção escolhida pelo usuário e 
+	//direciona para essa opção através do switch case.
+	char entradaChr;
 	
 	system("cls");
 	setlocale(LC_ALL, "Portuguese");
 	
 	printf("Menu Principal\n");
 	printf("C - Conversão\nS - Sistema Linear\nE - Equação Algébrica\nF - Finalizar\n");
-	scanf ("%2c", &menu);
-	
-	while(menu != 'C' && menu != 'S' && menu != 'E' && menu != 'F'){
-		printf("Alternativa inválida, tente novamente:\n");
-		scanf ("%2c", &menu);
-	}
-	switch (menu){
+	scanf (" %c", &entradaChr);                                                               
+
+	switch (entradaChr){
 		case 'C':
 			conversao();
 			break;
@@ -44,6 +52,10 @@ void chamar_menu(){
 		case 'F': 
 			printf("Até logo\n");
 			break;
+		default:
+			printf("Alternativa inválida.");
+			esperar_comando();
+			chamar_menu();
 	}
 }
 int main (){
